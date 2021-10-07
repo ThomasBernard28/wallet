09b31f9aea7eb92beaae5d29813c7fbf329e9164
Ugo Proietti <ugo.pr@protonmail.com>
2021-10-07 11:19:43 +0200

    test

--- app/src/test/java/wallet/AppTest.java
+++ app/src/test/java/wallet/AppTest.java
@@ -10,5 +10,6 @@ public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertEqual(1,0);
     }
 }
