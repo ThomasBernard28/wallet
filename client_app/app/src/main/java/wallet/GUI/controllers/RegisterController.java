1b8a27c0fa849117490baadf93257fd72223ade1
Harbiinger <theogodin51@gmail.com>
2022-03-20 14:15:59 +0100

    fixed back button in registerController

--- client_app/app/src/main/java/wallet/GUI/controllers/RegisterController.java
+++ client_app/app/src/main/java/wallet/GUI/controllers/RegisterController.java
@@ -3,6 +3,7 @@ package wallet.GUI.controllers;
 import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,6 +13,7 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.IOException;
+import java.io.File;
 
 public class RegisterController {
     @FXML
@@ -29,9 +31,10 @@ public class RegisterController {
 
     @FXML
     private void onBackButtonCLicked() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(new File("build/resources/main/GUI/fxml/hello-view.fxml").toURL());
+        Parent root = fxmlLoader.load();
         Stage stage = (Stage) (back.getScene().getWindow());
-        Scene scene = new Scene(fxmlLoader.load(), 320, 320);
+        Scene scene = new Scene(root, 320, 320);
         stage.setScene(scene);
         stage.show();
     }
