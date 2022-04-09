f4efb300d529c3c3f234890534491ae22fbe3358
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2022-04-09 12:52:01 +0200

    nothing

--- server_app/src/main/java/com/example/Api/account/AccountService.java
+++ server_app/src/main/java/com/example/Api/account/AccountService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import javax.swing.text.html.Option;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
