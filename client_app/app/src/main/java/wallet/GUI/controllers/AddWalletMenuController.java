359560368306ea17725d7eda6f3b9024c4e3da7a
Harbiinger <theogodin51@gmail.com>
2022-04-12 13:26:48 +0200

    add wallet combo box

--- client_app/app/src/main/java/wallet/GUI/controllers/AddWalletMenuController.java
+++ client_app/app/src/main/java/wallet/GUI/controllers/AddWalletMenuController.java
@@ -5,22 +5,47 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import javafx.collections.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import wallet.App;
+import wallet.APP.Bank;
+import wallet.APP.Wallet;
 
 public class AddWalletMenuController {
     @FXML
     Button back;
     @FXML
     Button add;
+    @FXML
+    ComboBox box;
+
+    @FXML
+    private void initialize() {
+        ArrayList<String> names = new ArrayList();
+        for (Bank b : App.banksList) {
+            boolean subscribed = false;
+            ArrayList<Wallet> walletsList = App.currentUser.get_walletsList();
+            for (Wallet w : walletsList) {
+                if (w.get_bic().equals(b.get_bic())) {
+                    subscribed = true;
+                }
+            }
+            if (!subscribed) {
+                names.add(b.get_name());
+            }
+        }
+        box.setItems(FXCollections.observableArrayList(names));
+    }
 
     @FXML
     private void onAddButtonClick() throws IOException {
-        App.currentUser.add_wallet("GKCCBEBB");
+        // App.currentUser.add_wallet("GKCCBEBB");
         onBackButtonClick();
     }
 
