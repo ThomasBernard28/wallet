5ee5dd9602c37c3a2f52b43323915a877a07eb7d
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2022-04-20 19:24:37 +0200

    fix coOwners

--- server_app/src/main/java/com/example/Api/account/AccountRepository.java
+++ server_app/src/main/java/com/example/Api/account/AccountRepository.java
@@ -20,8 +20,8 @@ public interface AccountRepository extends JpaRepository<Account, String> {
     @Query("SELECT s FROM ACCOUNTS s WHERE s.client = ?1 AND s.activity = 1")
     List<Account> findAccountByClient(Client client);
 
-    @Query(value = "SELECT * FROM ACCOUNTS WHERE walletID = ?1 AND activity =1 UNION (SELECT a.iban 'iban', a.walletID 'walletID', a.userID 'userID', a.bic 'bic', a.type 'type'," +
-            " a.avgBalance 'avgBalance', a.localCurr 'localCurr', a.activity 'activity' FROM ACCOUNTS a, CO_OWNER c WHERE a.iban = c.ibanOwner AND c.walletID_coOwner = ?1)", nativeQuery = true)
+    @Query(value = "SELECT * FROM ACCOUNTS WHERE walletID = ?1 AND activity =1 UNION\n" +
+            "(SELECT a.iban 'iban', c.walletID_coOwner 'walletID', a.userID 'userID', a.bic 'bic', a.type 'type', a.avgBalance 'avgBalance', a.localCurr 'localCurr', a.activity 'activity' FROM ACCOUNTS a, CO_OWNER c WHERE a.iban = c.ibanOwner AND c.walletID_coOwner = ?1))", nativeQuery = true)
     List<Account> findAccountByWalletID(Long walletID);
 
     @Query(value = "UPDATE ACCOUNTS SET walletID = ?1 WHERE iban = ?2", nativeQuery = true)
